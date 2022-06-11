186b23f5be815498024adaaf063c0f1a2393eff1
Jasitharan <jasitharan1998@gmail.com>
2022-06-11 15:30:53 +0530

    bug fixed

--- lib/screens/home_pages/profile_screen.dart
+++ lib/screens/home_pages/profile_screen.dart
@@ -50,165 +50,170 @@ class _ProfileScreenState extends State<ProfileScreen> {
           : SafeArea(
               child: SizedBox(
                 width: mediaQuery.size.width,
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      sizedBox20,
-                      Stack(
-                        children: [
-                          SizedBox(
-                            height: 150,
-                            width: 150,
-                            child: CircleAvatar(
-                                backgroundColor: Colors.white,
-                                child: _user.imageUrl == null
-                                    ? const Image(
-                                        image: AssetImage(
-                                            'assets/images/check.png'))
-                                    : CachedNetworkImage(
-                                        imageUrl: kBackendUrl + _user.imageUrl!,
-                                        fit: BoxFit.cover,
-                                        height: 100,
-                                        width: 100,
-                                        errorWidget: (context, url, error) =>
-                                            const Image(
-                                                image: AssetImage(
-                                                    'assets/images/check.png')),
-                                      )),
+                child: Form(
+                  key: _formKeyForSetting,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        sizedBox20,
+                        Stack(
+                          children: [
+                            SizedBox(
+                              height: 150,
+                              width: 150,
+                              child: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: _user.imageUrl == null
+                                      ? const Image(
+                                          image: AssetImage(
+                                              'assets/images/check.png'))
+                                      : CachedNetworkImage(
+                                          imageUrl:
+                                              kBackendUrl + _user.imageUrl!,
+                                          fit: BoxFit.cover,
+                                          height: 100,
+                                          width: 100,
+                                          errorWidget: (context, url, error) =>
+                                              const Image(
+                                                  image: AssetImage(
+                                                      'assets/images/check.png')),
+                                        )),
+                            ),
+                            Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      width: 4,
+                                      color: Theme.of(context)
+                                          .scaffoldBackgroundColor,
+                                    ),
+                                    color: Colors.green,
+                                  ),
+                                  child: InkWell(
+                                    onTap: () async {
+                                      setState(() {
+                                        _loading = true;
+                                      });
+
+                                      var result = await _auth.updateProfile(
+                                          _user.email,
+                                          _name,
+                                          ImageSource.gallery,
+                                          _user.uid);
+
+                                      if (result == null) {
+                                        showSnacBar(
+                                            context, 'Something went wrong');
+                                      } else {
+                                        _user.imageUrl = result['url_image'];
+                                        showSnacBar(
+                                            context, 'Successfully updated.');
+                                      }
+
+                                      setState(() {
+                                        _loading = false;
+                                      });
+                                    },
+                                    child: const Icon(
+                                      Icons.edit,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                )),
+                          ],
+                        ),
+                        sizedBox50,
+                        sizedBox20,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: ClassTextFormField(
+                            initialValue: _name,
+                            imageName: kUserIcon,
+                            hintText: 'Name',
+                            validator: (val) {
+                              final temp = val.toString().split(' ').join();
+                              return !isAlpha(temp) ? 'Enter an name' : null;
+                            },
+                            onChanged: (val) {
+                              _name = val;
+                            },
                           ),
-                          Positioned(
-                              bottom: 0,
-                              right: 0,
-                              child: Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    width: 4,
-                                    color: Theme.of(context)
-                                        .scaffoldBackgroundColor,
-                                  ),
-                                  color: Colors.green,
-                                ),
-                                child: InkWell(
-                                  onTap: () async {
-                                    setState(() {
-                                      _loading = true;
-                                    });
-
-                                    var result = await _auth.updateProfile(
-                                        _user.email,
-                                        _name,
-                                        ImageSource.gallery,
-                                        _user.uid);
-
-                                    if (result == null) {
-                                      showSnacBar(
-                                          context, 'Something went wrong');
-                                    } else {
-                                      _user.imageUrl = result['url_image'];
-                                      showSnacBar(
-                                          context, 'Successfully updated.');
-                                    }
-
-                                    setState(() {
-                                      _loading = false;
-                                    });
-                                  },
-                                  child: const Icon(
-                                    Icons.edit,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              )),
-                        ],
-                      ),
-                      sizedBox50,
-                      sizedBox20,
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: ClassTextFormField(
-                          initialValue: _name,
-                          imageName: kUserIcon,
-                          hintText: 'Name',
-                          validator: (val) {
-                            final temp = val.toString().split(' ').join();
-                            return !isAlpha(temp) ? 'Enter an name' : null;
-                          },
-                          onChanged: (val) {
-                            _name = val;
-                          },
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: ClassTextFormField(
-                          initialValue: _email,
-                          imageName: kEmailIcon2,
-                          hintText: 'Email',
-                          validator: (val) =>
-                              !isEmail(val!) ? 'Enter an email' : null,
-                          onChanged: (val) {
-                            _email = val;
-                          },
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ClassTextFormField(
+                            initialValue: _email,
+                            imageName: kEmailIcon2,
+                            hintText: 'Email',
+                            validator: (val) =>
+                                !isEmail(val!) ? 'Enter an email' : null,
+                            onChanged: (val) {
+                              _email = val;
+                            },
+                          ),
                         ),
-                      ),
-                      sizedBox40,
-                      SizedBox(
-                        width: 250,
-                        child: ClassicButton(
-                          title: 'Save',
-                          handler: () async {
-                            if (_formKeyForSetting.currentState!.validate()) {
+                        sizedBox40,
+                        SizedBox(
+                          width: 250,
+                          child: ClassicButton(
+                            title: 'Save',
+                            handler: () async {
+                              print('hello');
+                              if (_formKeyForSetting.currentState!.validate()) {
+                                setState(() {
+                                  _loading = true;
+                                });
+
+                                var result = await _auth.updateProfile(
+                                    _email, _name, null, _user.uid);
+
+                                if (result != null) {
+                                  showSnacBar(context, 'Successfully updated.');
+                                } else {
+                                  showSnacBar(context, 'Something went wrong');
+                                }
+
+                                setState(() {
+                                  _loading = false;
+                                });
+                              }
+                            },
+                          ),
+                        ),
+                        sizedBox20,
+                        SizedBox(
+                          width: 250,
+                          child: ClassicButton(
+                            title: 'User Reports',
+                            handler: () {
+                              Navigator.pushNamed(
+                                  context, UserReportsScreen.routeName);
+                            },
+                          ),
+                        ),
+                        sizedBox50,
+                        SizedBox(
+                          width: 250,
+                          child: ClassicButton(
+                            title: 'Logout',
+                            color: Colors.red,
+                            handler: () async {
                               setState(() {
                                 _loading = true;
                               });
-
-                              var result = await _auth.updateProfile(
-                                  _email, _name, null, _user.uid);
-
-                              if (result != null) {
-                                showSnacBar(context, 'Successfully updated.');
-                              } else {
-                                showSnacBar(context, 'Something went wrong');
-                              }
-
-                              setState(() {
-                                _loading = false;
-                              });
-                            }
-                          },
-                        ),
-                      ),
-                      sizedBox20,
-                      SizedBox(
-                        width: 250,
-                        child: ClassicButton(
-                          title: 'User Reports',
-                          handler: () {
-                            Navigator.pushNamed(
-                                context, UserReportsScreen.routeName);
-                          },
-                        ),
-                      ),
-                      sizedBox50,
-                      SizedBox(
-                        width: 250,
-                        child: ClassicButton(
-                          title: 'Logout',
-                          color: Colors.red,
-                          handler: () async {
-                            setState(() {
-                              _loading = true;
-                            });
-                            await _auth.signOut();
-                          },
-                        ),
-                      )
-                    ],
+                              await _auth.signOut();
+                            },
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
